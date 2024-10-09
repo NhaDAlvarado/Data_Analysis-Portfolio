cb5d0cf7e14b65c585a5454cb3dc09b90499ac8e
Mia Dang <miadang@Mias-MacBook-Pro.local>
2024-10-09 13:51:54 -0700

    completed insight analysis

--- Salary_Analysis/Insight_analysis.sql
+++ Salary_Analysis/Insight_analysis.sql
@@ -55,23 +55,125 @@ with avg_yoe as (
 )
 select job_title, avg_yoe 
 from avg_yoe
-where rank = 1 or rank = 108
+where rank = 1 or rank = 108;
 
--- 6. Is there a correlation between age and salary?
+-- 6. What is the average salary for each education level?
+select education_level, round(avg(salary),2) as avg_salary
+from employee_data
+group by education_level;
 
+-- 7. How many employees have more than 10 years of experience, and what are their job titles?
+select job_title, count(*) as employees_counts
+from employee_data
+where years_of_experience >10
+group by job_title; 
 
--- 7. What is the average salary for each education level?
--- 8. How many employees have more than 10 years of experience, and what are their job titles?
--- 9. What is the total salary expenditure by job title?
--- 10. What is the distribution of employees' age across different job titles?
--- 11. Which gender has a higher average salary across different job titles?
--- 12. How many employees with a PhD have a salary greater than $100,000?
--- 13. What is the average number of years of experience for employees with a salary greater than $100,000?
--- 14. How does the average salary differ between males and females across different education levels?
--- 15. What is the most common job title among employees with a Master’s degree?
--- 16. What is the salary range for employees under 30 years old?
--- 17. How many employees have salaries in the top 10% of the dataset?
--- 18. Which job titles have the highest concentration of employees with less than 5 years of experience?
--- 19. What is the salary distribution for employees aged over 40?
--- 20. How does the salary compare for employees with the same job title but different education levels?
+-- 8. What is the total salary expenditure by job title?
+select job_title, sum(salary) as expenses
+from employee_data
+group by job_title;
 
+-- 9. What is the distribution of employees' age across different job titles?
+with count_ppl_in_age_groups as (
+	select count(*) as num_ppl_in_group,
+		case when age <25 then '<25'
+				when age >=25 and age < 35 then '25-34'
+				when age >=35 and age < 45 then '35-44'
+				when age >=45 and age < 55 then '45-54'
+				when age >=55 then '55+'
+		end as age_groups
+	from employee_data
+	group by age_groups
+),
+count_ppl_per_title_in_group as (
+	select job_title, count(*) as ppl_per_title_in_group,
+	(case when age <25 then '<25'
+		when age >=25 and age < 35 then '25-34'
+		when age >=35 and age < 45 then '35-44'
+		when age >=45 and age < 55 then '45-54'
+		when age >=55 then '55+'
+	end) as age_groups
+	from employee_data
+	group by age_groups, job_title
+)
+select a.age_groups, job_title, 
+round(100.0* ppl_per_title_in_group/num_ppl_in_group ,2) as percentage
+from count_ppl_per_title_in_group as t 
+join count_ppl_in_age_groups as a on t.age_groups = a.age_groups
+order by age_groups;
+
+-- 10. Which gender has a higher average salary across different job titles?
+with ranking_avg_salary as (
+	select job_title, gender, round(avg(salary),2) as avg_salary,
+		rank() over (partition by job_title order by avg(salary) desc) as ranking 
+	from employee_data
+	group by job_title, gender
+)
+select job_title, gender, avg_salary
+from ranking_avg_salary
+where ranking =1; 
+
+-- 11. How many employees with a PhD have a salary greater than $100,000?
+select education_level, count(*) as num_of_phd
+from employee_data
+where education_level = 'PhD' and salary > 100000
+group by education_level;
+
+-- 12. What is the average number of years of experience for employees with a salary greater than $100,000?
+select round(avg(years_of_experience)) as avg_yoe
+from employee_data
+where salary >100000;
+
+-- 13. How does the average salary differ between males and females across different education levels?
+select education_level, gender, round(avg(salary),2) as avg_salary
+from employee_data
+group by education_level, gender 
+order by education_level, gender;
+
+-- 14. What is the most common job title among employees with a Master’s degree?
+select job_title, count(*) as num_of_ppl
+from employee_data
+where education_level = 'Master''s Degree'
+group by job_title 
+order by num_of_ppl desc;
+
+-- 15. What is the salary range for employees under 30 years old?
+select min(salary) as min_salary, max(salary) as max_salary 
+from employee_data
+where age <30;
+
+-- 16. How many employees have salaries in the top 10% of the dataset?
+with percentile_salary as (
+    select percentile_cont(0.9) within group (order by salary) as salary_90th
+    from employee_data
+)
+select count(*) as num_of_top_earners
+from employee_data, percentile_salary
+where salary >= salary_90th;
+
+-- 17. Which job titles have the highest concentration of employees with less than 5 years of experience?
+select job_title, count(*) as num_of_ppl 
+from employee_data
+where years_of_experience <5
+group by job_title
+order by num_of_ppl desc;
+
+-- 18. What is the salary distribution for employees aged over 40?
+select 
+	(case when salary <50000 then 'less than 50k'
+		when salary between 50000 and 99999 then '50k-100k'
+		when salary between 100000 and 149999 then '100k-150k'
+		when salary between 150000 and 199999 then '150k-200k'
+		else '200k+'
+	end) as salary_range,
+	count(*) as num_of_employees
+from employee_data
+where age >40 
+group by salary_range
+order by num_of_employees;
+
+-- 19. How does the salary compare for employees with the same job title but different education levels?
+select job_title, education_level, round(avg(salary),2) as avg_salary, round(avg(years_of_experience),2) as avg_yoe
+from employee_data
+group by job_title, education_level
+order by job_title, avg_salary desc 
