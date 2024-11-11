df2c7b0bd4d7263a44b12605b45b1ed1b9443a0d
Mia Dang <miadang@Mias-MacBook-Pro.local>
2024-11-11 08:16:14 -0800

    create table and import data

---
+++ Mental_Health_Analysis/Create_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE mental_health (
+    timestamp TIMESTAMP,
+    gender VARCHAR(50),
+    country VARCHAR(100),
+    occupation VARCHAR(100),
+    self_employed VARCHAR(10),
+    family_history VARCHAR(10),
+    treatment VARCHAR(10),
+    days_indoors VARCHAR(20),
+    growing_stress VARCHAR(10),
+    changes_habits VARCHAR(10),
+    mental_health_history VARCHAR(10),
+    mood_swings VARCHAR(20),
+    coping_struggles VARCHAR(10),
+    work_interest VARCHAR(10),
+    social_weakness VARCHAR(10),
+    mental_health_interview VARCHAR(10),
+    care_options VARCHAR(50)
+);
