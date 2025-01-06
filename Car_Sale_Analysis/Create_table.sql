2ec5c8b0fd59d2bde5991036b72114e4d325a5be
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-01-06 10:56:01 -0800

    created table and imported data

---
+++ Car_Sale_Analysis/Create_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE car_sales (
+    Car_id text,
+	Date date,
+	CustomerName varchar(50),
+	Gender varchar(20),
+	AnnualIncome int,
+	Dealer_Name text,
+	Company VARCHAR(100),
+    Model VARCHAR(100),
+	Engine text,
+	Transmission varchar(20),
+	Color varchar (30),
+	Price int,
+	Dealer_No text,
+	BodyStyle varchar (30),
+	Phone text,
+	Dealer_Region text
+);
+select * from car_sales 
