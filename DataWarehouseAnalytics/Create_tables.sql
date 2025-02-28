1e95df6fed4803b8057dfeb110ca702bccc7da16
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-02-28 11:56:48 -0800

    started 5 basic analytics methods

---
+++ DataWarehouseAnalytics/Create_tables.sql
@@ -0,0 +1,44 @@
+-- Create Schemas
+
+CREATE SCHEMA gold;
+
+CREATE TABLE gold.dim_customers(
+	customer_key int,
+	customer_id int,
+	customer_number varchar(50),
+	first_name varchar(50),
+	last_name varchar(50),
+	country varchar(50),
+	marital_status varchar(50),
+	gender varchar(50),
+	birthdate date,
+	create_date date
+);
+
+CREATE TABLE gold.dim_products(
+	product_key int ,
+	product_id int ,
+	product_number varchar(50) ,
+	product_name varchar(50) ,
+	category_id varchar(50) ,
+	category varchar(50) ,
+	subcategory varchar(50) ,
+	maintenance varchar(50) ,
+	cost int,
+	product_line varchar(50),
+	start_date date 
+);
+
+CREATE TABLE gold.fact_sales(
+	order_number varchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipping_date date,
+	due_date date,
+	sales_amount int,
+	quantity int,
+	price int 
+);
+
+select * from gold.fact_sales
