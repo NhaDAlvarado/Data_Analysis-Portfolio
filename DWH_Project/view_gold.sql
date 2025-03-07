c249cf57a937665650a21e2c4751f3b345297abc
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-07 12:23:23 -0800

    in progress

---
+++ DWH_Project/view_gold.sql
@@ -0,0 +1,38 @@
+-- create view gold.dim_customers as 
+-- 	select 
+-- 		row_number() over (order by cst_id) as customer_key, 
+-- 		ci.cst_id as customer_id,
+-- 		ci.cst_key as customer_name,
+-- 		ci.cst_firstname as first_name,    
+-- 		ci.cst_lastname as last_name,
+-- 		la.cntry as country,
+-- 		ci.cst_marital_status as marital_status,
+-- 		case when ci.cst_gndr != 'n/a' then ci.cst_gndr --crm is master table
+-- 			else coalesce(ca.gen, 'n/a')
+-- 		end as gender,
+-- 		ca.bdate as birthdate,
+-- 		ci.cst_create_date as create_date
+-- 	from silver.crm_cust_info as ci
+-- 	left join silver.erp_cust_az12 as ca
+-- 	on ci.cst_key = ca.cid
+-- 	left join silver.erp_loc_a101 la
+-- 	on la.cid = ci.cst_key;
+
+-- create view gold.dim_products as 
+-- 	select 
+-- 		row_number() over (order by pi.prd_start_dt, pi.prd_key ) as product_key,
+-- 		pi.prd_id as product_id,
+-- 		pi.prd_key as product_number,
+-- 		pi.prd_nm as product_name,
+-- 		pi.cat_id as category_id,
+-- 		pc.cat as category,
+-- 		pc.subcat as subcategory,
+-- 		pc.maintenance,
+-- 		pi.prd_cost as cost,	
+-- 		pi.prd_line as product_line,	
+-- 		pi.prd_start_dt as start_date
+-- 	from silver.crm_prod_info as pi
+-- 	left join silver.erp_px_cat_g1v2 as pc
+-- 	on pi.cat_id = pc.id 
+-- 	where prd_end_dt is null;
+
