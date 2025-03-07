5feb0be6620927f137a3f72d2838f774a42f84a0
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-07 13:03:45 -0800

    in progress

--- DWH_Project/gold_data_checking.sql
+++ DWH_Project/gold_data_checking.sql
@@ -31,9 +31,41 @@ left join silver.erp_cust_az12 as ca
 on ci.cst_key = ca.cid
 left join silver.erp_loc_a101 la
 on la.cid = ci.cst_key
-order by ci.cst_gndr, ca.gen	
+order by ci.cst_gndr, ca.gen;
 
+-- Filter out all historical data
+with filter_out as (
+	select 
+		pi.prd_id,
+		pi.cat_id,
+		pi.prd_key,
+		pi.prd_nm,
+		pi.prd_cost,	
+		pi.prd_line,	
+		pi.prd_start_dt,
+		pi.prd_end_dt,
+		pc.cat,
+		pc.subcat,
+		pc.maintenance
+	from silver.crm_prod_info as pi
+	left join silver.erp_px_cat_g1v2 as pc
+	on pi.cat_id = pc.id 
+	where prd_end_dt is null 
+)
+-- check duplicate after join
+select prd_key, count(*)
+from filter_out
+group by prd_key
+having count(*) >1; 
 
-
+-- foreign key integrity (dimensions)
+select * 
+from gold.fact_sales as f
+left join gold.dim_customers as c
+on c.customer_key = f.customer_key
+left join gold.dim_products as p
+on p.product_key = f.product_key
+where p.product_key is null
+	or c.customer_key is null;
 
 
