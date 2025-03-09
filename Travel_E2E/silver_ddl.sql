130988f09f933bc672a0e03c24a2d18d37df52eb
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-09 14:17:24 -0700

    in progress

---
+++ Travel_E2E/silver_ddl.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS silver.flights;
+CREATE TABLE silver.flights (
+    travelCode INT,
+    userCode INT,
+    from_location VARCHAR(50),
+    to_location VARCHAR(50),silver
+    flightType VARCHAR(50),
+    price FLOAT,
+    time FLOAT,  
+    distance FLOAT,    
+    agency VARCHAR(50),    
+    date DATE
+);
+
+DROP TABLE IF EXISTS silver.hotels;
+CREATE TABLE silver.hotels (
+    travelCode int,
+	userCode int,
+	name varchar(50),
+	place varchar(50),
+	days int,
+	price float,
+	total float,
+	date DATE
+);
+
+DROP TABLE IF EXISTS silver.users;
+CREATE TABLE silver.users (
+	code int,
+	company	varchar(50),
+	name varchar(50),
+	gender varchar(50),
+	age int	
+);
