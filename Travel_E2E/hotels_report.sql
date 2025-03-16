01ab83abfc9465695b2cc9f6af8fc5e9b6bf7194
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-16 11:59:59 -0700

    in progress

---
+++ Travel_E2E/hotels_report.sql
@@ -0,0 +1,23 @@
+/*
+===============================================================================
+Hotels Report
+===============================================================================
+Purpose:
+    - This report consolidates key hotels metrics and behaviors.
+
+Highlights:
+    1. Gathers essential fields such as hotel name, city, state, price, 
+		checkin date, 
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
+    3. Aggregates product-level metrics:
+       - total books
+       - total sales
+       - total customers (unique)
+       - retention rate 
+       - lifespan (in months)
+    4. Calculates valuable KPIs:
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
+===============================================================================
+*/
