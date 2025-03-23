76a54b99c20a7dc4c9011f4b0da02cd9263abc73
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-23 14:43:58 -0700

    completed EDA

--- Medicare_Analysis/Exploratory_Data_Analysis.sql
+++ Medicare_Analysis/Exploratory_Data_Analysis.sql
@@ -37,30 +37,3 @@ union all
 select 'Total Medicare Cover' as measure_name, sum(average_medicare_payments) from `bigquery-public-data.cms_medicare.inpatient_charges_2015`
 union all
 select 'Total Drug Discharge' as measure_name, sum(total_discharges) from `bigquery-public-data.cms_medicare.inpatient_charges_2015`;
-
--- 2. MAGNITUDE ANALYSIS
--- Total provider by state
-select provider_state,
-      count(distinct provider_name) as provider_cnt
-from `bigquery-public-data.cms_medicare.inpatient_charges_2015`
-group by provider_state;
-
--- Total drugs provided by provider
-select provider_name, count(distinct drg_definition)
-from `bigquery-public-data.cms_medicare.inpatient_charges_2015`
-group by provider_name;
-
--- Total discharges fro each drug 
-select drg_definition,
-  sum(total_discharges) as total_discharges
-from `bigquery-public-data.cms_medicare.inpatient_charges_2015`
-group by drg_definition;
-
--- Total hospital bills & avg charge for each provider
-select provider_name,
-  sum(average_covered_charges) as total_bill,
-  round(avg(average_covered_charges),2) as avg_charge
-from `bigquery-public-data.cms_medicare.inpatient_charges_2015`
-group by provider_name;
-
-
