4c15bd3bf50ce8f9d25c20aa6bfdea76d78e1ac9
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-31 09:39:44 -0700

    in progress

--- Medicare_Analysis/Reports.sql
+++ Medicare_Analysis/Reports.sql
@@ -15,3 +15,56 @@ Highlights:
 ===============================================================================
 */
 
+with yearly_info as (
+  select '2015' as year,
+        drg_definition, 
+        sum(total_discharges) as ttl_discharges,
+        sum(average_covered_charges) as ttl_hospital_charged, 
+        sum(average_total_payments) as ttl_patient_paid,
+        sum(average_medicare_payments) as ttl_medicare_covered
+  from `bigquery-public-data.cms_medicare.inpatient_charges_2015` 
+  group by drg_definition
+  union all 
+  select '2014' as year,
+        drg_definition, 
+        sum(total_discharges) as ttl_discharges,
+        sum(average_covered_charges) as ttl_hospital_charged, 
+        sum(average_total_payments) as ttl_patient_paid,
+        sum(average_medicare_payments) as ttl_medicare_covered
+  from `bigquery-public-data.cms_medicare.inpatient_charges_2014` 
+  group by drg_definition
+  union all 
+  select '2013' as year,
+          drg_definition, 
+        sum(total_discharges) as ttl_discharges,
+        sum(average_covered_charges) as ttl_hospital_charged, 
+        sum(average_total_payments) as ttl_patient_paid,
+        sum(average_medicare_payments) as ttl_medicare_covered
+  from `bigquery-public-data.cms_medicare.inpatient_charges_2013` 
+  group by drg_definition
+  union all 
+  select '2012' as year,
+          drg_definition, 
+        sum(total_discharges) as ttl_discharges,
+        sum(average_covered_charges) as ttl_hospital_charged, 
+        sum(average_total_payments) as ttl_patient_paid,
+        sum(average_medicare_payments) as ttl_medicare_covered
+  from `bigquery-public-data.cms_medicare.inpatient_charges_2012` 
+  group by drg_definition
+  union all 
+  select '2011' as year,
+          drg_definition, 
+        sum(total_discharges) as ttl_discharges,
+        sum(average_covered_charges) as ttl_hospital_charged, 
+        sum(average_total_payments) as ttl_patient_paid,
+        sum(average_medicare_payments) as ttl_medicare_covered
+  from `bigquery-public-data.cms_medicare.inpatient_charges_2011` 
+  group by drg_definition
+)
+select year, 
+        drg_definition, 
+        ttl_discharges,
+        ttl_hospital_charged, 
+        ttl_patient_paid, 
+        ttl_medicare_covered
+from yearly_info
