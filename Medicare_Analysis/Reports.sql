bfc8187c7a65d964f5097a52983d3e30d4818604
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-03-30 19:39:04 -0700

    in progress

---
+++ Medicare_Analysis/Reports.sql
@@ -0,0 +1,17 @@
+/*
+===============================================================================
+Report
+===============================================================================
+Purpose:
+    - This report consolidates key metrics and behaviors.
+
+Highlights:
+    1. Gathers essential fields such as drg_definitions, state, and cost (2011-2015).
+    2. Segments products by revenue to identify 'High Medicare Dependency', 
+        Moderate Medicare Dependency, or Low Medicare Dependency.
+    3. Aggregates product-level metrics:
+       - total medicare cover
+       - total hospital bill
+===============================================================================
+*/
+
