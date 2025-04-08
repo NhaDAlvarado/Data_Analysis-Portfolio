e89be599b752781a1132f0c688173db1707a8498
Mia Dang <miadang@Mias-MacBook-Pro.local>
2025-04-08 12:50:28 -0700

    in progress

--- Nursing_Facility_Analysis/Advance_Analysis.sql
+++ Nursing_Facility_Analysis/Advance_Analysis.sql
@@ -1,15 +1,31 @@
--- EDA
+-- EDA 
 -- How many facility in the dataset
 select count(distinct facility_name) as num_facilities
-from `bigquery-public-data.cms_medicare.nursing_facilities_2013`;
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
 
 -- Explore all states 
 select count(distinct state) as num_states
-from `bigquery-public-data.cms_medicare.nursing_facilities_2013`;
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
 
 select distinct state
-from `bigquery-public-data.cms_medicare.nursing_facilities_2013`;
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
 
 -- Explore total patitens from all nursing facility
 select sum(distinct_beneficiaries_per_provider)
-from `bigquery-public-data.cms_medicare.nursing_facilities_2013`;
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
+
+-- Explore avg snf chrage amount all over facility
+select round(avg(total_snf_charge_amount),2)
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
+
+-- Explore avg snf medicare allowed amount all over facility
+select round(avg(total_snf_medicare_allowed_amount),2)
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
+
+-- Explore avg snf medicare payment amount all over facility
+select round(avg(total_snf_medicare_payment_amount),2)
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
+
+-- Explore avg snf medicare standard payment amount all over facility
+select round(avg(total_snf_medicare_standard_payment_amount),2)
+from `bigquery-public-data.cms_medicare.nursing_facilities_2014`;
